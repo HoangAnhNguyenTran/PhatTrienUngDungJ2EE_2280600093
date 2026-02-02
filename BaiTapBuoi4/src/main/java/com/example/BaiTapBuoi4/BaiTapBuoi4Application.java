cf4b060646cdf095c1fad7bec34d148fa9d833eb
HoangAnhNguyenTran <thienxado@gmail.com>
2026-02-02 15:51:42 +0700

    Add BaiTapBuoi4

---
+++ BaiTapBuoi4/src/main/java/com/example/BaiTapBuoi4/BaiTapBuoi4Application.java
@@ -0,0 +1,13 @@
+package com.example.BaiTapBuoi4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiTapBuoi4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaiTapBuoi4Application.class, args);
+	}
+
+}
---
+++ BaiTapBuoi4/src/test/java/com/example/BaiTapBuoi4/BaiTapBuoi4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BaiTapBuoi4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiTapBuoi4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
